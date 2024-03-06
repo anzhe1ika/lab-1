688227dea8c9a0d693c3ad9a36f0ce09c5549187
anzhe1ika <162430723+anzhe1ika@users.noreply.github.com>
2024-03-06 11:18:23 +0200

    Add files via upload

---
+++ Lab1.2(06.03)/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, List<string>> dic = new Dictionary<int, List<string>>();
+        dic.Add(1, new List<string>(1) { "i" });
+        dic.Add(2, new List<string>(2) { "you", "we", "they" });
+        dic.Add(3, new List<string>(3) { "he", "she", "it" });
+
+        int count = 0;
+        foreach (var value in dic.Values)
+        {
+            if (value is List<string>)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("The number of entries in the dictionary whose values are a list: " + count);
+        Console.WriteLine("-------------------------------------------------------------------");
+        Console.WriteLine("Writing a dictionary in JSON format: ");
+
+        string json = JsonConvert.SerializeObject(dic, Formatting.Indented);
+        Console.WriteLine(json);
+
+
+        Console.ReadKey();
+    }
+
+}
+
---
+++ Lab1.3(06.03)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+        list.Add("lncksfnvk");
+        list.Add("icfnvkn");
+        list.Add("kncksfnvk");
+        list.Add("acfnvkn");
+
+        var agg = list.Aggregate("", (current, next) => current + next[0]);
+        Console.WriteLine(agg);
+
+        Console.ReadKey();
+    }
+
+}
