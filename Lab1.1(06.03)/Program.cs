dbf0667c582df41c480de9c255c37255a3165c58
anzhe1ika <162430723+anzhe1ika@users.noreply.github.com>
2024-03-06 11:16:25 +0200

    Add files via upload

---
+++ Lab1.1(06.03)/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "C:\\Users\\User\\source\\repos\\Lab1.1(06.03)\\file.txt";
+
+        List<int> list = new List<int>();
+
+        StreamReader sr = new StreamReader(path);
+        string s;
+
+        try
+        {
+            while ((s = sr.ReadLine()) != null)
+            {
+                list.Add(s.Length);
+            }
+
+            sr.Close();
+
+            Console.WriteLine("Number of characters in each line:");
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        catch (Exception ex) { Console.WriteLine("Error: " + ex.Message); }
+
+        Console.ReadKey();
+    }
+}
